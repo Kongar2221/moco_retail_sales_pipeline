b1a62197dd26bfb4b5e7b280b1a29358a1e37802
Kongar2221 <163117163+Kongar2221@users.noreply.github.com>
2025-08-28 13:38:36 +0300

    yml file for start, *will need changing*

--- dbt_moco_retail/target/run/moco_retail/models/core/fct_monthly_sales.sql
+++ dbt_moco_retail/target/run/moco_retail/models/core/fct_monthly_sales.sql
@@ -1,16 +1,16 @@
 
         
             delete from "moco_retail"."main"."fct_monthly_sales" as DBT_INCREMENTAL_TARGET
-            using "fct_monthly_sales__dbt_tmp20250827223109197505"
+            using "fct_monthly_sales__dbt_tmp20250828133420142903"
             where (
                 
-                    "fct_monthly_sales__dbt_tmp20250827223109197505".sale_date = DBT_INCREMENTAL_TARGET.sale_date
+                    "fct_monthly_sales__dbt_tmp20250828133420142903".sale_date = DBT_INCREMENTAL_TARGET.sale_date
                     and 
                 
-                    "fct_monthly_sales__dbt_tmp20250827223109197505".item_code = DBT_INCREMENTAL_TARGET.item_code
+                    "fct_monthly_sales__dbt_tmp20250828133420142903".item_code = DBT_INCREMENTAL_TARGET.item_code
                     and 
                 
-                    "fct_monthly_sales__dbt_tmp20250827223109197505".supplier = DBT_INCREMENTAL_TARGET.supplier
+                    "fct_monthly_sales__dbt_tmp20250828133420142903".supplier = DBT_INCREMENTAL_TARGET.supplier
                     
                 
                 
@@ -21,6 +21,6 @@
     insert into "moco_retail"."main"."fct_monthly_sales" ("sale_date", "supplier", "item_code", "item_description", "item_type", "retail_sales", "retail_transfers", "warehouse_sales", "total_sales")
     (
         select "sale_date", "supplier", "item_code", "item_description", "item_type", "retail_sales", "retail_transfers", "warehouse_sales", "total_sales"
-        from "fct_monthly_sales__dbt_tmp20250827223109197505"
+        from "fct_monthly_sales__dbt_tmp20250828133420142903"
     )
   
